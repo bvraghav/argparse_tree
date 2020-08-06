d2f9ff300bceb72651df8adbdc2a4baab6b69a4e
B.V. Raghav <bvraghav@gmail.com>
2020-08-06 20:54:46 +0530

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as F :
 
 setup(
   name                          = 'argparse_tree',
-  version                       = '0.1.3',
+  version                       = '0.1.3a',
   description                   = (
     'Split parse args into filesystem tree for '
     'complex projects.'
